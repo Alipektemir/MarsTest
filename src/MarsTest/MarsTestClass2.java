d6a498e8785f97c28a7b7d917efe5f5dd956f8b0
Alipektemir <Alipektemir>
2020-03-17 22:50:19 -0400

    Add existing file

--- src/MarsTest/MarsTestClass2.java
+++ src/MarsTest/MarsTestClass2.java
@@ -1,10 +1,10 @@
 package MarsTest;
 
-public class MarstTest {
+public class MarsTestClass2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("Mars Test class2");
 	}
 
 }
